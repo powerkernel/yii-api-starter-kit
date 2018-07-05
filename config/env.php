17b8fd198adec6b0a06019732957d00460106e0c
Harry Tang <harry@powerkernel.com>
2018-07-05 09:12:30 +0700

    dynamic env config

--- config/env.php
+++ config/env.php
@@ -5,5 +5,5 @@
  * @copyright Copyright (c) 2018 Power Kernel
  */
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', file_exists(__DIR__.'/prod')?false:true);
+defined('YII_ENV') or define('YII_ENV', file_exists(__DIR__.'/prod')?'prod':'dev');
